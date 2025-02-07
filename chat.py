ea425242a47a316267ecc7a67adfcb1375760841
EveMoon7 <wumengla1212@gmail.com>
2025-02-08 03:34:15 +0800

    chat.py

--- chat.py
+++ chat.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 
-TOKEN = '(DISCORD_BOT_TOKEN)'
+TOKEN = ('DISCORD_BOT_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True  # 啟用讀取訊息的權限
