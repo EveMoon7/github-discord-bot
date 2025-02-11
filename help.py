ec68c2debd5edbe1e55edd14facb13c35b4f0410
EveMoon7 <wumengla1212@gmail.com>
2025-02-11 11:07:09 +0800

    描述您的更改內容

--- help.py
+++ help.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = Trues
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='>', help_command=None, intents=intents)
 
